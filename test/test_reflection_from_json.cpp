8ccb83fdb8bf50baa6854c597f88bc28dbc4538c
Small_Orangeee <3314500052@qq.com>
2025-07-27 23:48:15 +0800

    实现json基础类型、嵌套类型反序列化

--- test/test_reflection_from_json.cpp
+++ test/test_reflection_from_json.cpp
@@ -8,22 +8,37 @@
 using namespace rapidjson;
 using namespace std;
 
+struct Address {
+    string m_street;
+    string m_city;
+};
+
 struct Person {
     bool m_bool;
+    int m_int;
+    Address m_address;
 };
 
 int main() {
-    const char json[] = " { \"m_bool\" : false }";
+    const char json[] = " { \"m_bool\" : false, \"m_int\" : 1, \"m_address\" : { \"m_street\" : \"Street\", \"m_city\" : \"City\" }";
 
     Person person;
     person.m_bool = true;
+    person.m_int = 2;
+    std::cout << "before: " << (person.m_bool ? "true" : "false") << endl;
+    std::cout << "before: " << person.m_int << endl;
     static auto member_offset_map = tinyrefl::struct_member_offset_map<Person>();
     
-    tinyrefl::ReaderHandler<Person> handle(member_offset_map, person);
+    // tinyrefl::ReaderHandler<Person> handle(member_offset_map, person);
+    tinyrefl::DispatchHandler handle;
+    handle.push_handler(member_offset_map, person);
 
     Reader reader;
     StringStream ss(json);
     reader.Parse(ss, handle);
-    std::cout << (person.m_bool ? "true" : "false") << endl;
+    std::cout << "parse after: " << (person.m_bool ? "true" : "false") << endl;
+    std::cout << "parse after: " << person.m_int << endl;
+    std::cout << "parse after: " << person.m_address.m_street << endl;
+    std::cout << "parse after: " << person.m_address.m_city << endl;
     return 0;
 }
--- tinyrefl/rapidjson_SAX.hpp
+++ tinyrefl/rapidjson_SAX.hpp
@@ -5,59 +5,19 @@
 #include <iostream>
 
 namespace tinyrefl {
+    // declear reader
+    template <typename T>
+    struct ReaderHandler;
+
     template <typename T, size_t... Is>
-    struct ReaderHandlerImp : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, ReaderHandlerImp<T, Is...>> {
-        using Tuple = decltype(struct_members_to_tuple<T>());
-        using ValueType = decltype(get_variant_type<T, Tuple, Is...>());
-        using MapType = frozen::unordered_map<frozen::string, ValueType, members_count_v<T>>;
-    public:
-        ReaderHandlerImp(const MapType& map_value, const T& value) : _struct_member_offset_map(map_value), _value(value) {}
+    struct ReaderHandlerImp;
 
-    public:
-        bool Null() { 
-            if (_iterator != _struct_member_offset_map.end()) {
-                // TODO
-            }
-            return true;
-        }
-        bool Bool(bool b) {
-            if (_iterator != _struct_member_offset_map.end()) {
-                auto offset = _iterator->second;
-                std::visit([&](auto arg) {
-                    using Value_Type = typename decltype(arg)::type;
-                    // static_assert(remove_cvref_t<Value_Type> == bool, "type mismatch");
-                    Value_Type& member_value = *reinterpret_cast<Value_Type*>(
-                         reinterpret_cast<char*>(const_cast<T*>(&_value)) + arg.value
-                    );
-                    member_value = b;
-                    printf("type name: [%s]\n", typeid(Value_Type).name());
-                }, offset);
-            }
-            return true;
-        }
-        bool Int(int i) { return true; }
-        bool Uint(unsigned u) { return true; }
-        bool Int64(int64_t i) { return true; }
-        bool Uint64(uint64_t u) { return true; }
-        bool Double(double d) { return true; }
-        bool String(const char* str, rapidjson::SizeType length, bool copy) { return true; }
-        bool StartObject() { std::cout << "StartObject" << std::endl; return true; }
-        bool Key(const char* str, rapidjson::SizeType length, bool copy) {
-            _iterator = _struct_member_offset_map.find(frozen::string(str, length));
-            std::cout << "Key: " << str << std::endl;
-            return true;
-        }
-        bool EndObject(rapidjson::SizeType memberCount) { std::cout << "EndObject" << std::endl; return true; }
-        bool StartArray() { return true; }
-        bool EndArray(rapidjson::SizeType elementCount) { return true; }
+    class DispatchHandler;
+    // Can convert assignment
+    template <typename Target, typename From>
+    constexpr bool is_json_compatible_v = std::is_assignable_v<Target&, From>;
 
-    private:
-        const MapType& _struct_member_offset_map;
-        MapType::const_iterator _iterator;
-        const T& _value;
-    };
-
-    template <typename T, typename IndexSeq>
+        template <typename T, typename IndexSeq>
     struct ReaderHandlerHelper;
 
     template <typename T, size_t... Is>
@@ -82,4 +42,190 @@ namespace tinyrefl {
             : Base(map_value, value) {}
     };
 
-} // end ttinyrefl namespace
+    struct IHandler {
+        virtual bool Null() = 0;
+        virtual bool Bool(bool) = 0;
+        virtual bool Int(int) = 0;
+        virtual bool Uint(unsigned) = 0;
+        virtual bool Int64(int64_t) = 0;
+        virtual bool Uint64(uint64_t) = 0;
+        virtual bool Double(double) = 0;
+        virtual bool RawNumber(const char* str, rapidjson::SizeType length, bool copy) = 0;
+        virtual bool String(const char*, rapidjson::SizeType, bool) = 0;
+        virtual bool StartObject() = 0;
+        virtual bool Key(const char*, rapidjson::SizeType, bool) = 0;
+        virtual bool EndObject(rapidjson::SizeType) = 0;
+        virtual bool StartArray() = 0;
+        virtual bool EndArray(rapidjson::SizeType) = 0;
+
+        virtual ~IHandler() = default;
+    };
+
+    class DispatchHandler : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, DispatchHandler> {
+    public:
+        template <typename T>
+        void push_handler(const typename ReaderHandler<T>::MapType& map, const T& value) {
+            auto* h = new ReaderHandler<T>(map, value);
+            h->set_dispatcher(this);
+            _stack.emplace_back(h);
+        }
+
+        void pop_handler() {
+            delete _stack.back();
+            _stack.pop_back();
+        }
+    public:
+        bool Bool(bool b)        { return top()->Bool(b); }
+        bool Int(int i)          { return top()->Int(i); }
+        bool Key(const char* s, rapidjson::SizeType l, bool c) { return top()->Key(s, l, c); }
+        bool Null() { return top()->Null(); }
+        bool Uint(unsigned u) { return top()->Uint(u); }
+
+        bool Int64(int64_t i) { return top()->Int64(i); }
+
+        bool Uint64(uint64_t u) { return top()->Uint64(u); }
+        bool Double(double d) { return top()->Double(d); }
+        bool RawNumber(const char* str, rapidjson::SizeType length, bool copy) { return top()->RawNumber(str, length, copy); }
+        bool String(const char* str, rapidjson::SizeType length, bool copy) { return top()->String(str, length, copy); }
+        bool StartArray() { return top()->StartArray(); }
+        bool EndArray(rapidjson::SizeType elementCount) { return top()->EndArray(elementCount); }
+        bool StartObject() { return top()->StartObject(); }
+        bool EndObject(rapidjson::SizeType m) {
+            bool result = top()->EndObject(m);
+            pop_handler();
+            return result;
+        }
+        
+    private:
+        IHandler* top() const { return _stack.back(); }
+
+    private:
+        std::vector<IHandler*> _stack;
+    };
+
+    // rapidjson::BaseReaderHandler<rapidjson::UTF8<>, ReaderHandlerImp<T, Is...>>
+    template <typename T, size_t... Is>
+    struct ReaderHandlerImp : public IHandler {
+        using Tuple = decltype(struct_members_to_tuple<T>());
+        using ValueType = decltype(get_variant_type<T, Tuple, Is...>());
+        using MapType = frozen::unordered_map<frozen::string, ValueType, members_count_v<T>>;
+    public:
+        ReaderHandlerImp(const MapType& map_value, const T& value) : _struct_member_offset_map(map_value), _value(value) {}
+
+    public:
+        bool Null() { 
+            if (_iterator != _struct_member_offset_map.end()) {
+                // TODO
+            }
+            return true;
+        }
+
+        bool Bool(bool b) override {
+            assign_if_match<bool>([&](auto& member) {
+                member = b;
+            });
+            return true;
+        }
+
+        bool Int(int i) override {
+            assign_if_match<int>([&](auto& member) {
+                member = i;
+            });
+            return true;
+        }
+
+        bool Uint(unsigned u) override {
+            assign_if_match<unsigned>([&](auto& member) {
+                member = u;
+            });
+            return true;
+        }
+
+        bool Int64(int64_t i) override {
+            assign_if_match<int64_t>([&](auto& member) {
+                member = i;
+            });
+            return true;
+        }
+
+        bool Uint64(uint64_t u) override {
+            assign_if_match<uint64_t>([&](auto& member) {
+                member = u;
+            });
+            return true;
+        }
+        bool Double(double d) override {
+            assign_if_match<double>([&](auto& member) {
+                member = d;
+            });
+            return true;
+        }
+        bool RawNumber(const char* str, rapidjson::SizeType length, bool copy) override {
+            assign_if_match<const char*>([&](auto& member) {
+                member = str;
+            });
+            return true;
+        }
+        bool String(const char* str, rapidjson::SizeType length, bool copy) override {
+            assign_if_match<const char*>([&](auto& member) {
+                member = str;
+            });
+            return true;
+        }
+        bool StartObject() override {
+            if (!_is_first_member) {
+                if (_iterator != _struct_member_offset_map.end()) {
+                    auto offset = _iterator->second;
+                    std::visit([&](auto arg) {
+                        using Value_Type = typename decltype(arg)::type;
+                        if constexpr (is_custom_type_v<Value_Type>) {
+                            static auto member_offset_map = struct_member_offset_map<Value_Type>();
+                            Value_Type& member_value = *reinterpret_cast<Value_Type*>(
+                                reinterpret_cast<char*>(const_cast<T*>(&_value)) + arg.value
+                            );
+                            _dispatch_handler->push_handler<Value_Type>(member_offset_map, member_value);
+                        }
+                    }, offset);
+                }
+            }
+            _is_first_member = false;
+            return true;
+        }
+        bool Key(const char* str, rapidjson::SizeType length, bool copy) override {
+            _iterator = _struct_member_offset_map.find(frozen::string(str, length));
+            std::cout << "Key: " << str << std::endl;
+            return true;
+        }
+        bool EndObject(rapidjson::SizeType memberCount) override { std::cout << "EndObject" << std::endl; return true; }
+        bool StartArray() override { return true; }
+        bool EndArray(rapidjson::SizeType elementCount) override { return true; }
+
+    private:
+        template <typename TargetType, typename F>
+        void assign_if_match(F&& assign_func) {
+            if (_iterator != _struct_member_offset_map.end()) {
+                auto offset = _iterator->second;
+                std::visit([&](auto arg) {
+                    using Value_Type = typename decltype(arg)::type;
+                    if constexpr (is_json_compatible_v<remove_cvref_t<Value_Type>, TargetType>) {
+                        Value_Type& member_value = *reinterpret_cast<Value_Type*>(
+                            reinterpret_cast<char*>(const_cast<T*>(&_value)) + arg.value
+                        );
+                        assign_func(member_value);
+                    }
+                }, offset);
+            }
+        }
+    
+    public:
+        void set_dispatcher(class DispatchHandler* dispatcher) { _dispatch_handler = dispatcher; }
+
+    private:
+        const MapType& _struct_member_offset_map;
+        MapType::const_iterator _iterator = nullptr;
+        const T& _value;
+        bool _is_first_member = true;
+        DispatchHandler* _dispatch_handler = nullptr;
+    };
+
+} // end ttinyrefl namespace 
